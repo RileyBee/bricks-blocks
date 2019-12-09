8550e8991740c6d0d6c370d7dafffec6a21a5d7f
Jochs1989 <43485024+Jochs1989@users.noreply.github.com>
2019-12-09 08:02:34 -0500

    Update Project_version_1.java

--- project_version_1/src/project_version_1/Project_version_1.java
+++ project_version_1/src/project_version_1/Project_version_1.java
@@ -134,102 +134,102 @@ public class Project_version_1 {
         newJFrame.setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         newJFrame.getContentPane().setLayout(null);
 
-        jLabel3.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        jLabel3.setFont(new java.awt.Font("Tahoma", 0, 14)); 
         jLabel3.setText(fmt.format(cementCost));
         newJFrame.getContentPane().add(jLabel3);
         jLabel3.setBounds(780, 330, 90, 20);
 
-        jLabel4.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        jLabel4.setFont(new java.awt.Font("Tahoma", 0, 14));
         jLabel4.setText("DEC 12, 2019");
         newJFrame.getContentPane().add(jLabel4);
         jLabel4.setBounds(750, 130, 120, 20);
 
-        jLabel5.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        jLabel5.setFont(new java.awt.Font("Tahoma", 0, 14));
         jLabel5.setText("DEC 15, 2019");
         newJFrame.getContentPane().add(jLabel5);
         jLabel5.setBounds(620, 200, 120, 20);
 
-        jLabel6.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        jLabel6.setFont(new java.awt.Font("Tahoma", 0, 14));
         jLabel6.setText("arch cost");
         newJFrame.getContentPane().add(jLabel6);
         jLabel6.setBounds(780, 350, 90, 20);
 
-        jLabel7.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        jLabel7.setFont(new java.awt.Font("Tahoma", 0, 14));
         jLabel7.setText("STATE TAX (0.7%)");
         newJFrame.getContentPane().add(jLabel7);
         jLabel7.setBounds(640, 470, 120, 20);
 
-        jLabel8.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        jLabel8.setFont(new java.awt.Font("Tahoma", 0, 14));
         jLabel8.setText("Amount Of Bricks: " + Integer.toString(bricksNeeded) + " Bricks");
         newJFrame.getContentPane().add(jLabel8);
         jLabel8.setBounds(240, 310, 510, 20);
 
-        jLabel9.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        jLabel9.setFont(new java.awt.Font("Tahoma", 0, 14));
         jLabel9.setText("Amount Of Mortar: " + Integer.toString(cementNeeded) + " Cubic Feet");
         newJFrame.getContentPane().add(jLabel9);
         jLabel9.setBounds(240, 330, 500, 20);
 
-        jLabel11.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        jLabel11.setFont(new java.awt.Font("Tahoma", 0, 14));
         jLabel11.setText("Time & Labor");
         newJFrame.getContentPane().add(jLabel11);
         jLabel11.setBounds(240, 370, 510, 20);
 
-        jLabel10.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        jLabel10.setFont(new java.awt.Font("Tahoma", 0, 14));
         jLabel10.setText("Arch Design Cost");
         newJFrame.getContentPane().add(jLabel10);
         jLabel10.setBounds(240, 350, 520, 20);
 
-        jLabel13.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        jLabel13.setFont(new java.awt.Font("Tahoma", 0, 14));
         jLabel13.setText(fmt.format(brickCost));
         newJFrame.getContentPane().add(jLabel13);
         jLabel13.setBounds(780, 310, 90, 20);
 
-        jLabel14.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        jLabel14.setFont(new java.awt.Font("Tahoma", 0, 14));
         jLabel14.setText("BRICKS");
         newJFrame.getContentPane().add(jLabel14);
         jLabel14.setBounds(20, 310, 90, 20);
 
-        jLabel15.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        jLabel15.setFont(new java.awt.Font("Tahoma", 0, 14));
         jLabel15.setText("MORTAR");
         newJFrame.getContentPane().add(jLabel15);
         jLabel15.setBounds(20, 330, 90, 20);
 
-        jLabel16.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        jLabel16.setFont(new java.awt.Font("Tahoma", 0, 14));
         jLabel16.setText("ARCH DESIGN");
         newJFrame.getContentPane().add(jLabel16);
         jLabel16.setBounds(20, 350, 90, 20);
 
-        jLabel17.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        jLabel17.setFont(new java.awt.Font("Tahoma", 0, 14));
         jLabel17.setText("LABOR");
         newJFrame.getContentPane().add(jLabel17);
         jLabel17.setBounds(20, 370, 90, 20);
 
-        jLabel19.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        jLabel19.setFont(new java.awt.Font("Tahoma", 0, 14));
         jLabel19.setText(fmt.format(3084));
         newJFrame.getContentPane().add(jLabel19);
         jLabel19.setBounds(780, 370, 90, 20);
 
-        jLabel21.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        jLabel21.setFont(new java.awt.Font("Tahoma", 0, 14));
         jLabel21.setText("state tax");
         newJFrame.getContentPane().add(jLabel21);
         jLabel21.setBounds(780, 470, 90, 20);
 
-        jLabel22.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        jLabel22.setFont(new java.awt.Font("Tahoma", 0, 14));
         jLabel22.setText("Total Cost");
         newJFrame.getContentPane().add(jLabel22);
         jLabel22.setBounds(780, 560, 90, 20);
 
-        jLabel24.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        jLabel24.setFont(new java.awt.Font("Tahoma", 0, 14));
         jLabel24.setText("subtotal");
         newJFrame.getContentPane().add(jLabel24);
         jLabel24.setBounds(780, 440, 90, 20);
 
-        jLabel2.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        jLabel2.setFont(new java.awt.Font("Tahoma", 0, 14));
         jLabel2.setText("Mr. Teter");
         newJFrame.getContentPane().add(jLabel2);
         jLabel2.setBounds(50, 200, 70, 30);
 
-        jLabel1.setIcon(new javax.swing.ImageIcon("C:\\Users\\Null\\Desktop\\bricks-blocks-bricks-blocks-v1\\JavaSchoolBuilderInvoice.jpg")); // NOI18N
+        jLabel1.setIcon(new javax.swing.ImageIcon("C:\\Users\\Null\\Desktop\\bricks-blocks-bricks-blocks-v1\\JavaSchoolBuilderInvoice.jpg"));
         newJFrame.getContentPane().add(jLabel1);
         jLabel1.setBounds(0, 0, 920, 710);
 
@@ -237,7 +237,6 @@ public class Project_version_1 {
         newJFrame.setVisible(true);
         newJFrame.setSize(915, 875);
         newJFrame.setLocation(500, 100);
-        
     }
     
 }
